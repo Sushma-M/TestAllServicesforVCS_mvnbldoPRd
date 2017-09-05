868e526d3f4f2b5c8d6c02df50971e25e3089b92
sushma 1Vc <sushma.maddirala+1Vcs@wavemaker.com>
2017-09-05 07:48:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS_mvnbldoPRd",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS_mvnbldoPRd",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
